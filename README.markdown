388bf939164a18e15831b57cbebc2fb9053e9dcb
yksk <yoko@carbonfairy.org>
2009-02-05 20:44:55 +0900

    * 書き直した。IEでのもたつき解消など  * テスト追加  * サイズ指定のインターフェース変更  * READMEちゃんと書いた

---
+++ README.markdown
@@ -0,0 +1,63 @@
+jquery.rack.js
+==============
+
+A interface like a iTunes' Grid View.
+
+- [Sample](http://codefairy.org/misc/jquery.rack.js/sample/)
+
+## Usage
+Replace the &lt;ul&gt; list to grid view interface:
+
+	<ul id="rack">
+		<li><a href="1"><img src="1.png"/></a></li>
+		<li><a href="2"><img src="2.png"/></a></li>
+		<li><a href="3"><img src="3.png"/></a></li>
+	</ul>
+	
+	<script type="text/javascript" src="jquery.js"></script>
+	<script type="text/javascript" src="jquery.rack.js"></script>
+	<script type="text/javascript">
+	$('#rack').rack();
+	</script>
+
+### Requirements
+- jQuery >= 1.2.3
+
+## Changes
+### 0.2
+- Rewrite
+  - Simply
+  - Faster on IE
+- Add Test (using [QUnit](http://docs.jquery.com/QUnit))
+
+### 0.1
+- Initial release
+
+## Authors
+- [yksk](http://twitter.com/yksk) <<yoko@carbonfairy.org>>
+
+## License
+The MIT License
+
+Copyright (c) 2008- yksk
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+## See
+- <http://carbonfairy.org/note/coding/081211-jquery-rack.html>
--- jquery.rack.js
+++ jquery.rack.js
@@ -1,40 +1,56 @@
-
+/*
+ * jquery.rack.js <http://github.com/yoko/jquery.rack.js/tree/master>
+ *
+ * (c) 2008- yksk <http://carbonfairy.org/>
+ * Released under the MIT license <http://www.opensource.org/licenses/mit-license.php>
+ */
 (function($) {
 	$.fn.rack = function(size) {
-		size = size || [this.width(), this.height()];
-		$(this).css({
-			overflow     : 'hidden',
-			width        : size[0],
-			height       : size[1],
-			textAlign    : 'center',
-			verticalAlign: 'middle'
-		});
+		var self = this;
+		size = size || {
+			width : this.width(),
+			height: this.height()
+		};
+		var li = $('li', this);
 
-		var d = size[0] / $('img', this).length;
-		var images = [];
+		var items = [];
 		var a, img;
-		$('li', this).each(function(i) {
-			var _a = $('a', this);
-			var _img = $('img', this);
-			images.push({
-				src : _img.attr('src'),
-				href: _a.attr('href')
-			});
-			if (!i) {
-				a = _a;
-				img = _img;
-			}
-			else $(this).remove();
-		});
 
-		this.bind('mousemove', function(e) {
-			var offset = $(this).offset({ scroll: false });
-			var x = e.pageX - offset.left;
-			var image = images[parseInt(x / d)];
-			if (image) {
-				a.attr('href', image.href);
-				img.attr('src', image.src);
-			}
-		});
+		var i = li.length;
+		var w = size.width / i;
+		while (i--) {
+			var current = li.eq(i);
+			a   = $('a', current).get(0);
+			img = $('img', current).get(0);
+
+			items.unshift({
+				href: a.href,
+				src : img.src
+			});
+			if (i > 0) current.remove();
+		}
+
+		return this
+			.css({
+				overflow     : 'hidden',
+				width        : size.width,
+				height       : size.height,
+				textAlign    : 'center',
+				verticalAlign: 'middle'
+			})
+			.mousemove(function(e, data) {
+				var self = $(this);
+
+				var offset = self.offset({ scroll: false });
+				var x = (data || e).pageX - offset.left;
+				var i = Math.floor(x / w);
+
+				if (i == self.data('index')) return;
+				self.data('index', i);
+
+				var item = items[i];
+				a.href = item.href;
+				img.src = item.src;
+			});
 	};
 })(jQuery);
